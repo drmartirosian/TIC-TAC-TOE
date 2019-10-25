068a155268fa0aafac2a9a63d32d7c931d85d3e2
MyMac <mymac@MacBook-Pro-10.local>
2019-10-25 13:27:46 -0500

    JS

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 /*----- constants -----*/
-const ;
+const winCOUNT = 3;
+const putMark = 1;
+const xoARRAY = ['X', 'O', 'X', 'O', 'X', 'O', 'X'];
+
+
 
 /*----- app's state (variables) -----*/
-
+let = start;
+let = turn;
+let = win;
 
 /*----- cached element references -----*/
-
+//resetButton
+//xoButton
+//messagePROMPTS
 
 /*----- event listeners -----*/
-
+//win
+//xoButton
+//
 
 /*----- functions -----*/
 
