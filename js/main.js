22b3b4aa4d663d0c36bfd7e193bd94b96560fde1
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2019-10-26 13:44:59 -0500

    listener for reset, centered page

--- js/main.js
+++ js/main.js
@@ -1,29 +1,47 @@
-/*----- constants -----*/
-// const winSCENARIOS = [
-//     '123', '456', '789', '147', '256', '369', '159', '357'
-// ];
+// /*----- constants ----------------------*/
+// const x = [];
+// const o = [];
 // const gameBoard = [];
+// const winSCENARIOS = [
+//     '123', 
+//     '456', 
+//     '789', 
+//     '147', 
+//     '256', 
+//     '369', 
+//     '159', 
+//     '357'
+// ];
 
-/*----- app's state (variables) -----*/
-// let = 
+// /*----- app's state (variables) -------*/
+// let win = a;
 
 
 /*----- cached element references -----*/
-//resetButton
+//TILE BUTTONS
 const tileAll = document.querySelectorAll('.tiles');
-console.log(tileAll);
+//RESET BUTTON
+const resetButton = document.querySelectorAll('.reset');
 
-/*----- event listeners -----*/
+
+/*------------ event listeners ---------*/
+//LISTENER FOR CLICK
 tileAll.forEach(element => {
     element.addEventListener('click', tileClick);
- 
-    
+});
+//LISTENER FOR RESET
+resetButton.forEach(element => {
+    element.addEventListener('click', resetListener);
 });
 
-/*----- functions -----*/
-//setupBoard
-//takingTurns
-//toWinLose
+
+/*------------ functions ----------------*/
+//TILE BUTTON DOES THIS
 function tileClick(evt) {
-    console.log('TEST : ', evt.target);
+    console.log(evt.target);
+};
+
+//RESET BUTTON DOES THIS:
+function resetListener(evt) {
+    console.log(evt.target);
 };
