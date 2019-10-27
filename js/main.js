36844522cad5cc34575c489c13a08bf51d56ac64
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2019-10-26 20:33:18 -0500

    connected clicks to array

--- js/main.js
+++ js/main.js
@@ -1,47 +1,43 @@
-// /*----- constants ----------------------*/
-// const x = [];
-// const o = [];
-// const gameBoard = [];
-// const winSCENARIOS = [
-//     '123', 
-//     '456', 
-//     '789', 
-//     '147', 
-//     '256', 
-//     '369', 
-//     '159', 
-//     '357'
-// ];
+/*------------ constants --------------*/
+var turn = [];
 
-// /*----- app's state (variables) -------*/
-// let win = a;
-
+/*----- app's state (variables) -------*/
 
 /*----- cached element references -----*/
 //TILE BUTTONS
 const tileAll = document.querySelectorAll('.tiles');
-//RESET BUTTON
-const resetButton = document.querySelectorAll('.reset');
-
 
 /*------------ event listeners ---------*/
 //LISTENER FOR CLICK
 tileAll.forEach(element => {
     element.addEventListener('click', tileClick);
 });
-//LISTENER FOR RESET
-resetButton.forEach(element => {
-    element.addEventListener('click', resetListener);
-});
-
 
 /*------------ functions ----------------*/
-//TILE BUTTON DOES THIS
-function tileClick(evt) {
-    console.log(evt.target);
+function tileClick() { 
+    turn.push('')
+    console.log(turn.length);
 };
 
-//RESET BUTTON DOES THIS:
-function resetListener(evt) {
-    console.log(evt.target);
-};
+// 
+
+
+
+
+
+  //TEST SITE:
+  //     for (turn = 0; turn <= 9; turn++) { //+1 to array every click...
+//     };
+// };
+
+        //player 1 starts game-odd numbered turns
+        // if (turn.length % 2 !== 0) {
+        // turn.push(1);
+        // };
+
+        //player 2 follows after -even numbered turns
+        // if (turn.length % 2 === 0) {
+        // turn.push(1);
+        // };
+
+
