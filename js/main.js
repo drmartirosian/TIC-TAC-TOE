202debc03c2ab363f96eb4e6af9cd0ba3e34f225
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2019-10-31 04:51:41 -0500

    win mechanics seem mostly functional... refine it in the future.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
+
+
+
+
+
+
+
 // Display an empty tic-tac-toe board when the page is initially displayed.--CHECK
+
 // A player can click on the nine cells to make a move.--CHECK
+
 // Every click will alternate between marking an X and O.--CHECK
-// Once occupied with an X or O, the cell cannot be played again.
+
+// Once occupied with an X or O, the cell cannot be played again. --CHECK
+
 // Provide a Reset Game button that will clear the contents of the board.--CHECK
 
 
 /*------------ constants --------------*/
-const content = document.getElementById('message');//for message...
-
 const win = [                  //possible win scenarios...
             [1, 2, 3],
             [4, 5, 6],
@@ -21,16 +30,15 @@ const win = [                  //possible win scenarios...
             [3, 5, 7],
 ];
 
-
 /*----- app's state (variables) -------*/
 var turn = []; //number of turns in game...
 var playerO = []; //
 var playerX = []; //
 
-
 /*----- cached element references -----*/
 //TILE BUTTONS
 const tileAll = document.querySelectorAll('.tiles');
+const content = document.getElementById('message');//for message...
 
 /*------------ event listeners ---------*/
 //LISTENER FOR CLICK
@@ -38,35 +46,43 @@ tileAll.forEach(element => {
     element.addEventListener('click', tileClick);
 });
 
-
-
-
-
-
-
 /*------------ functions ----------------*/
 //STATES OF GAMEPLAY...
-function tileClick(event) { 
+function tileClick(event) {
     turn.push(''); //+1 to turn array per click
     let tileLabeler = event.target;
+    //PLAYER1
+    if (turn.length % 2 !== 0 && turn.length <= 9) { 
+      message.textContent = "PLAYER2'S TURN!";
+      tileLabeler.innerHTML = "X";
+      playerX.push(parseInt(tileLabeler.id));
+      playerX.sort();
+      //PLAYER X win conditions
+      let win = checkWin(playerX);
+      if (win) {
+        message.textContent = "Player-1 Won!";
+      }
+    } else { //PLAYER2
+      message.textContent = "PLAYER1'S TURN!";
+      tileLabeler.innerHTML = "O";
+      playerO.push(parseInt(tileLabeler.id));
+      playerO.sort();
+      
+      //PLAYER O win conditions
+      let win = checkWin(playerO);
+      if (win) {
+        message.textContent = ("Player-2 Won!");
+      }
+    }
+    if (turn.length >= 9) {
+      return message.textContent = "GAME OVER!"
+    }
+  };
 
-    if (turn.length % 2 !== 0 && turn.length <= 9) { //PLAYER1
-        message.textContent = "PLAYER2'S TURN!";
-        tileLabeler.innerHTML = "X";
-        playerX.push(tileLabeler.id);
-        playerX.sort();
-        console.log(playerX);
+function checkWin(playerArray) {
+    return win.some((winArray) => winArray.every((winNumber) => playerArray.includes(winNumber)));
+}
 
-    } if (turn.length % 2 === 0 && turn.length <= 9) { //PLAYER2
-        message.textContent = "PLAYER1'S TURN!";
-        tileLabeler.innerHTML = "O";
-        playerO.push(tileLabeler.id);
-        playerO.sort();
-        console.log(playerO);
 
-    } if (turn.length >= 9) 
-        message.textContent = "DRAW!";
-        // console.log(turn.length); 
-};
 
-console.log(JSON.stringify(win));
+
